7b38b5223d6a6bc2913e19846ea92636b0b665da
fukusuke1026 <fukusuke.takahashi@gmail.com>
2021-01-24 09:53:40 +0900

    ch02 done.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let s_num = rand::thread_rng().gen_range(1, 101);
+    println!("you guessed: {}", s_num);
+    
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed to read line.");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&s_num) {
+            Ordering::Less => println!("A"),
+            Ordering::Greater => println!("B"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }   
+}
